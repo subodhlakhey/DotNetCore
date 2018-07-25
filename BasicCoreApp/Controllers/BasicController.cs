4362726e3bb3dfa48ac404661e354e590b1406e0
Subodh <abcd@yahoo.com>
2018-07-25 11:40:00 -0400

    Add project files.

---
+++ BasicCoreApp/Controllers/BasicController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicCoreApp.Controllers
+{
+    
+    
+    public class BasicController : Controller
+    {
+        [HttpGet]
+        [Route("api/basicApp")]
+        public IActionResult App()
+        {
+            return Ok("Basic DotNet Core Application");
+        }
+    }
+}
